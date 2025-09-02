5f44855d34b306e341b398dcd557309cfb151c17
Eric Cote <eric@coteexpert.com>
2025-09-02 03:01:25 -0400

    update ProductGallery types

--- src/ui/components/ProductGallery.tsx
+++ src/ui/components/ProductGallery.tsx
@@ -1,29 +1,29 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 "use client";
 
 import React, { useState } from "react";
 import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, FreeMode, Thumbs, Zoom } from "swiper/modules";
+import type { Swiper as SwiperType } from "swiper"; // Import the core Swiper type
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/thumbs";
 import "swiper/css/zoom";
 
-//SwiperCore.use([Navigation, Thumbs, FreeMode, Zoom]);
-
 type ImageItem = { url: string; alt?: string };
 
 export function ProductGallery({ images }: { images: ImageItem[] }) {
 	// store thumbs swiper instance
-	const [thumbsSwiper, setThumbsSwiper] = useState<typeof Swiper | null>(null);
+	const [swiperInstance, setSwiperInstance] = useState<SwiperType | null>(null);
 
 	return (
 		<div>
 			<Swiper
 				navigation
-				spaceBetsddween={4}
+				spaceBetween={4}
 				slidesPerView={1}
-				thumbs={{ swiper: thumbsSwiper }}
+				thumbs={{ swiper: swiperInstance }}
 				modules={[FreeMode, Zoom, Navigation, Thumbs]}
 				style={{ width: "100%", height: 560 }}
 			>
@@ -44,7 +44,7 @@ export function ProductGallery({ images }: { images: ImageItem[] }) {
 			<Swiper
 				modules={[FreeMode, Navigation, Thumbs]}
 				watchSlidesProgress
-				onSwiper={(s) => setThumbsSwiper(s)}
+				onSwiper={setSwiperInstance}
 				spaceBetween={5}
 				slidesPerView={7}
 				freeMode={true}
