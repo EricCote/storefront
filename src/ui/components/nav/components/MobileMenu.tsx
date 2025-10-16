f7fc8a6c3e3eab0523c96209c430cba1e3bd26cf
Eric Cote <eric@coteexpert.com>
2025-10-16 14:25:06 -0400

    @headlessui/react v2 changed component names

--- src/ui/components/nav/components/MobileMenu.tsx
+++ src/ui/components/nav/components/MobileMenu.tsx
@@ -1,11 +1,11 @@
-"use client";
+'use client';
 
-import { Fragment, type ReactNode } from "react";
-import { Dialog, Transition } from "@headlessui/react";
-import { Logo } from "../../Logo";
-import { useMobileMenu } from "./useMobileMenu";
-import { OpenButton } from "./OpenButton";
-import { CloseButton } from "./CloseButton";
+import { Fragment, type ReactNode } from 'react';
+import { Transition, Dialog, DialogPanel, TransitionChild } from '@headlessui/react';
+import { Logo } from '../../Logo';
+import { useMobileMenu } from './useMobileMenu';
+import { OpenButton } from './OpenButton';
+import { CloseButton } from './CloseButton';
 
 type Props = {
 	children: ReactNode;
@@ -16,39 +16,40 @@ export const MobileMenu = ({ children }: Props) => {
 
 	return (
 		<>
-			<OpenButton onClick={openMenu} aria-controls="mobile-menu" />
+			<OpenButton onClick={openMenu} aria-controls='mobile-menu' />
 			<Transition show={isOpen}>
 				<Dialog onClose={closeMenu}>
-					<Dialog.Panel className="fixed inset-0 z-20 flex h-dvh w-screen flex-col overflow-y-scroll">
-						<Transition.Child
-							className="sticky top-0 z-10 flex h-16 shrink-0 bg-neutral-100/50 px-3 backdrop-blur-md sm:px-8"
-							enter="motion-safe:transition-all motion-safe:duration-150"
-							enterFrom="bg-transparent"
-							enterTo="bg-neutral-100"
-							leave="motion-safe:transition-all motion-safe:duration-150"
-							leaveFrom="bg-neutral-100"
-							leaveTo="bg-transparent"
+					<DialogPanel className='fixed inset-0 z-20 flex h-dvh w-screen flex-col overflow-y-scroll'>
+						<TransitionChild
+							as='div'
+							className='sticky top-0 z-10 flex h-16 shrink-0 bg-neutral-100/50 px-3 backdrop-blur-md sm:px-8'
+							enter='motion-safe:transition-all motion-safe:duration-150'
+							enterFrom='bg-transparent'
+							enterTo='bg-neutral-100'
+							leave='motion-safe:transition-all motion-safe:duration-150'
+							leaveFrom='bg-neutral-100'
+							leaveTo='bg-transparent'
 						>
 							<Logo />
-							<CloseButton onClick={closeMenu} aria-controls="mobile-menu" />
-						</Transition.Child>
-						<Transition.Child
+							<CloseButton onClick={closeMenu} aria-controls='mobile-menu' />
+						</TransitionChild>
+						<TransitionChild
 							as={Fragment}
-							enter="motion-safe:transition-all motion-safe:duration-150"
-							enterFrom="opacity-0 -translate-y-3 bg-transparent"
-							enterTo="opacity-100 translate-y-0 bg-white"
-							leave="motion-safe:transition-all motion-safe:duration-150"
-							leaveFrom="opacity-100 translate-y-0 bg-white"
-							leaveTo="opacity-0 -translate-y-3 bg-transparent"
+							enter='motion-safe:transition-all motion-safe:duration-150'
+							enterFrom='opacity-0 -translate-y-3 bg-transparent'
+							enterTo='opacity-100 translate-y-0 bg-white'
+							leave='motion-safe:transition-all motion-safe:duration-150'
+							leaveFrom='opacity-100 translate-y-0 bg-white'
+							leaveTo='opacity-0 -translate-y-3 bg-transparent'
 						>
 							<ul
-								className="flex h-full flex-col divide-y divide-neutral-200 whitespace-nowrap p-3 pt-0 sm:p-8 sm:pt-0 [&>li]:py-3"
-								id="mobile-menu"
+								className='flex h-full flex-col divide-y divide-neutral-200 p-3 pt-0 whitespace-nowrap sm:p-8 sm:pt-0 [&>li]:py-3'
+								id='mobile-menu'
 							>
 								{children}
 							</ul>
-						</Transition.Child>
-					</Dialog.Panel>
+						</TransitionChild>
+					</DialogPanel>
 				</Dialog>
 			</Transition>
 		</>
--- src/ui/components/nav/components/UserMenu/UserMenu.tsx
+++ src/ui/components/nav/components/UserMenu/UserMenu.tsx
@@ -1,13 +1,13 @@
-"use client";
+'use client';
 
-import { Fragment } from "react";
-import clsx from "clsx";
-import { Menu, Transition } from "@headlessui/react";
-import { UserInfo } from "./components/UserInfo";
-import { UserAvatar } from "./components/UserAvatar";
-import { type UserDetailsFragment } from "@/gql/graphql";
-import { logout } from "@/app/actions";
-import { LinkWithChannel } from "@/ui/atoms/LinkWithChannel";
+import { Fragment } from 'react';
+import clsx from 'clsx';
+import { Menu, MenuButton, MenuItems, MenuItem, Transition } from '@headlessui/react';
+import { UserInfo } from './components/UserInfo';
+import { UserAvatar } from './components/UserAvatar';
+import { type UserDetailsFragment } from '@/gql/graphql';
+import { logout } from '@/app/actions';
+import { LinkWithChannel } from '@/ui/atoms/LinkWithChannel';
 
 type Props = {
 	user: UserDetailsFragment;
@@ -15,55 +15,55 @@ type Props = {
 
 export function UserMenu({ user }: Props) {
 	return (
-		<Menu as="div" className="relative">
-			<Menu.Button className="relative flex rounded-full bg-neutral-200 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-neutral-800">
-				<span className="sr-only">Open user menu</span>
+		<Menu as='div' className='relative'>
+			<MenuButton className='relative flex rounded-full bg-neutral-200 text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-neutral-800 focus:outline-none'>
+				<span className='sr-only'>Open user menu</span>
 				<UserAvatar user={user} />
-			</Menu.Button>
+			</MenuButton>
 			<Transition
 				as={Fragment}
-				enter="transition ease-out duration-100"
-				enterFrom="transform opacity-0 scale-95"
-				enterTo="transform opacity-100 scale-100"
-				leave="transition ease-in duration-75"
-				leaveFrom="transform opacity-100 scale-100"
-				leaveTo="transform opacity-0 scale-95"
+				enter='transition ease-out duration-100'
+				enterFrom='transform opacity-0 scale-95'
+				enterTo='transform opacity-100 scale-100'
+				leave='transition ease-in duration-75'
+				leaveFrom='transform opacity-100 scale-100'
+				leaveTo='transform opacity-0 scale-95'
 			>
-				<Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right divide-y divide-neutral-200 bg-white py-1 text-start shadow ring-1 ring-neutral-200 ring-opacity-5 focus:outline-none">
+				<MenuItems className='ring-opacity-5 absolute right-0 z-10 mt-2 w-48 origin-top-right divide-y divide-neutral-200 bg-white py-1 text-start shadow ring-1 ring-neutral-200 focus:outline-none'>
 					<UserInfo user={user} />
-					<div className="flex flex-col px-1 py-1">
-						<Menu.Item>
+					<div className='flex flex-col px-1 py-1'>
+						<MenuItem>
 							{({ active }) => (
 								<LinkWithChannel
-									href="/orders"
+									href='/orders'
 									className={clsx(
-										active && "bg-neutral-100",
-										"block px-4 py-2 text-sm font-medium text-neutral-500 hover:text-neutral-700",
+										active && 'bg-neutral-100',
+										'block px-4 py-2 text-sm font-medium text-neutral-500 hover:text-neutral-700',
 									)}
 								>
 									My orders
 								</LinkWithChannel>
 							)}
-						</Menu.Item>
+						</MenuItem>
 					</div>
-					<div className="flex flex-col px-1 py-1">
-						<Menu.Item>
+					<div className='flex flex-col px-1 py-1'>
+						<MenuItem>
 							{({ active }) => (
 								<form action={logout}>
 									<button
-										type="submit"
+										type='submit'
 										className={clsx(
-											active && "bg-neutral-100",
-											"block px-4 py-2 text-start text-sm font-medium text-neutral-500 hover:text-neutral-700",
+											active && 'bg-neutral-100',
+											'block px-4 py-2 text-start text-sm font-medium text-neutral-500 hover:text-neutral-700',
 										)}
 									>
 										Log Out
 									</button>
 								</form>
 							)}
-						</Menu.Item>
+						</MenuItem>
 					</div>
-				</Menu.Items>
+				</MenuItems>
 			</Transition>
 		</Menu>
 	);
