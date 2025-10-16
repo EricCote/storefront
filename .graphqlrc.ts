0ec2f73745926bf66bb964c7760badb5c97d7417
Eric Cote <eric@coteexpert.com>
2025-10-16 13:26:23 -0400

    Make graphql compatible to saleor 3.22

--- .graphqlrc.ts
+++ .graphqlrc.ts
@@ -1,50 +1,51 @@
-import { loadEnvConfig } from "@next/env";
-import type { CodegenConfig } from "@graphql-codegen/cli";
+import { loadEnvConfig } from '@next/env';
+import type { CodegenConfig } from '@graphql-codegen/cli';
 
 loadEnvConfig(process.cwd());
 
 let schemaUrl = process.env.NEXT_PUBLIC_SALEOR_API_URL;
 
-if (process.env.GITHUB_ACTION === "generate-schema-from-file") {
-	schemaUrl = "schema.graphql";
+if (process.env.GITHUB_ACTION === 'generate-schema-from-file') {
+	schemaUrl = 'schema.graphql';
 }
 
 if (!schemaUrl) {
 	console.error(
-		"Before GraphQL types can be generated, you need to set NEXT_PUBLIC_SALEOR_API_URL environment variable.",
+		'Before GraphQL types can be generated, you need to set NEXT_PUBLIC_SALEOR_API_URL environment variable.',
 	);
-	console.error("Follow development instructions in the README.md file.");
+	console.error('Follow development instructions in the README.md file.');
 	process.exit(1);
 }
 
 const config: CodegenConfig = {
 	overwrite: true,
 	schema: schemaUrl,
-	documents: "src/graphql/**/*.graphql",
+	documents: 'src/graphql/**/*.graphql',
 	generates: {
-		"src/gql/": {
-			preset: "client",
+		'src/gql/': {
+			preset: 'client',
 			plugins: [],
 			config: {
-				documentMode: "string",
+				documentMode: 'string',
 				useTypeImports: true,
 				strictScalars: true,
 				scalars: {
-					Date: "string",
-					DateTime: "string",
-					Day: "number",
-					Decimal: "number",
-					GenericScalar: "unknown",
-					JSON: "unknown",
-					JSONString: "string",
-					Metadata: "Record<string, string>",
-					Hour: "number",
-					Minute: "number",
-					PositiveDecimal: "number",
-					UUID: "string",
-					Upload: "unknown",
-					WeightScalar: "unknown",
-					_Any: "unknown",
+					Date: 'string',
+					DateTime: 'string',
+					Day: 'number',
+					Decimal: 'number',
+					GenericScalar: 'unknown',
+					JSON: 'unknown',
+					JSONString: 'string',
+					Metadata: 'Record<string, string>',
+					Hour: 'number',
+					Minute: 'number',
+					PositiveDecimal: 'number',
+					PositiveInt: 'number',
+					UUID: 'string',
+					Upload: 'unknown',
+					WeightScalar: 'unknown',
+					_Any: 'unknown',
 				},
 			},
 			presetConfig: {
